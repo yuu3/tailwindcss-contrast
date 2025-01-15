34cddd145e736d15b34407d98a456927037aeca3
yuu3 <yunosuke0802@icloud.com>
2025-01-15 22:47:38 +0900

    ref

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="sticky top-0 w-full h-14">
+    <header className="sticky top-0 w-full h-14 bg-white">
       <div className="w-full max-w-3xl h-full mx-auto px-4 grid items-center">
         <h1>Tailwind Contrast Checker</h1>
       </div>

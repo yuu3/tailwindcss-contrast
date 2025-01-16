2481d10fad4de4810d35652176add48d49f6e58b
yuu3 <yunosuke0802@icloud.com>
2025-01-16 22:44:27 +0900

    ref

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,17 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="TailwindCSS Contrast Checker" />
         <meta property="og:description" content="Contrast checker for TailwindCSS." />
-        <meta property="og:image" content="ogp.png" />
+        <meta property="og:image" content="/ogp.png" />
         <meta property="og:url" content="https://tailwindcss-contrast.vercel.app/" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name='twitter:title' content='TailwindCSS Contrast Checker' />
         <meta name='twitter:description' content='Contrast checker for TailwindCSS.' />
-        <meta name='twitter:image' content='ogp-card.png' />
+        <meta name='twitter:image' content='/ogp-card.png' />
       </Head>
       <body className="min-h-screen flex flex-col">
         <Header />

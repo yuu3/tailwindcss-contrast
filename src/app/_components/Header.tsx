6fe9bb6267b2103d5548eac6a32aedb0e79393c3
yuu3 <yunosuke0802@icloud.com>
2025-02-03 22:17:33 +0900

    add: logo

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image"
+
 export const Header = () => {
   return (
     <header className="sticky top-0 w-full h-14 bg-white">
-      <div className="w-full max-w-3xl h-full mx-auto px-4 grid items-center">
-        <h1>Tailwind Contrast Checker</h1>
+      <div className="w-full max-w-3xl h-full mx-auto px-4 flex items-center justify-between">
+        <h1 className="flex items-center gap-x-4">
+          <Image src="/logo.png" alt="Tailwind Contrast Checker" width={32} height={32} />
+          Tailwind Contrast Checker
+        </h1>
+        <a
+          href="https://github.com/yuu3/tailwindcss-contrast"
+          target="_blank"
+        >
+          <Image
+            src="/github.svg"
+            alt="GitHubのロゴ"
+            width={32}
+            height={32}
+          />
+        </a>
       </div>
     </header>
   )
